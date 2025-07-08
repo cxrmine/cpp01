50edc4ae0b1d6b9784a1225d95afda05b855a9a1
cxrmine <amaryllismain09@gmail.com>
2025-07-08 13:57:42 +0200

    chore: add randomChump.cpp file

---
+++ ex00/randomChump.cpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmalkawi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 13:54:02 by tmalkawi          #+#    #+#             */
+/*   Updated: 2025/07/08 13:54:02 by tmalkawi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+void randomChump(std::string name) {
+  if (name.empty()) {
+    std::cout << "Error: please use a valid name for the zombie" << '\n';
+    return;
+  }
+}
