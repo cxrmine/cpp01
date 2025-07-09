ea145c4c10e09ad336aa9d9757c1834df0ac08ee
cxrmine <amaryllismain09@gmail.com>
2025-07-09 11:03:09 +0200

    chore: add Zombie files from the previous exercice

---
+++ ex01/Zombie.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmalkawi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 12:58:45 by tmalkawi          #+#    #+#             */
+/*   Updated: 2025/07/08 12:58:45 by tmalkawi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+#include <iostream>
+
+Zombie::Zombie(std::string name) {
+  if (name.empty()) {
+    std::cout << "Error: please use a bvalid name for the zombie" << '\n';
+    return;
+  }
+
+  Zombie::name = name;
+}
+
+Zombie::~Zombie() {
+  std::cout << "[DEBUG] Zombie \"" << Zombie::name
+            << "\" has died (destroyed in c++ terms)"
+            << "\n";
+}
+
+void Zombie::announce(void) {
+  std::cout << Zombie::name << ": " << "BraiiiiiiinnnzzzZ..." << '\n';
+}
---
+++ ex01/Zombie.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmalkawi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/08 12:58:22 by tmalkawi          #+#    #+#             */
+/*   Updated: 2025/07/08 12:58:22 by tmalkawi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string>
+
+class Zombie {
+public:
+  Zombie(std::string name);
+  ~Zombie();
+
+  void announce(void);
+
+private:
+  std::string name;
+};
+
+void randomChump(std::string name);
+Zombie *newZombie(std::string name);
