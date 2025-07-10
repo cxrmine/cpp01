cd2995c8bba964b6cd74e6d3e10a435170d9e431
cxrmine <amaryllismain09@gmail.com>
2025-07-10 13:24:32 +0200

    feat: base case of HumanB

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -9,3 +9,13 @@
 /*   Updated: 2025/07/10 13:11:58 by tmalkawi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "HumanB.hpp"
+#include "Weapon.hpp"
+#include <iostream>
+#include <string>
+
+HumanB::HumanB(std::string name) : name(name) {
+  if (name.empty())
+    return;
+}
