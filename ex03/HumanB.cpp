d0bfac9e54bd07e331b5fbf99a6479025199bded
cxrmine <amaryllismain09@gmail.com>
2025-07-10 13:15:37 +0200

    chore: remove header code in cpp file

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -9,21 +9,3 @@
 /*   Updated: 2025/07/10 13:11:58 by tmalkawi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "Weapon.hpp"
-#include <string>
-
-#ifndef HUMANB_HPP
-#define HUMANB_HPP
-
-class HumanB {
-public:
-  HumanB();
-  ~HumanB();
-
-private:
-  Weapon weapon;
-  std::string name;
-};
-
-#endif // !HUMANB_HPP
