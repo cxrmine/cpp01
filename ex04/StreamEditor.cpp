f4ea6e306e73997a78ae57ace65b84a8d4788120
cxrmine <amaryllismain09@gmail.com>
2025-07-17 16:56:23 +0200

    feat: create input file

--- ex04/StreamEditor.cpp
+++ ex04/StreamEditor.cpp
@@ -11,11 +11,10 @@
 /* ************************************************************************** */
 
 #include "StreamEditor.hpp"
-#include <algorithm>
 #include <cstddef>
+#include <cstring>
 #include <fstream>
 #include <iostream>
-#include <iterator>
 #include <string>
 
 StreamEditor::StreamEditor(const char *inputFile, const char *s1,
@@ -40,10 +39,23 @@ std::size_t tm_strlen(const char *s) {
   return (i);
 }
 
+std::string tm_replace(std::size_t index, std::size_t len, char *s1,
+                       const char *s2) {
+  if (len == 0)
+    return s1;
+  if (s1[0] == '\0' || s2[0] == '\0')
+    return s1;
+
+  for (std::size_t i = index; i < len; i++) {
+    s1[i] = s2[i];
+  }
+
+  return s2;
+}
+
 std::string StreamEditor::changePattern(std::string line, const char *s1,
                                         const char *s2) {
-  std::size_t counter = 0;
-  bool changeTrigger = false;
+  std::size_t index = 0;
 
   if (s1 == NULL || s2 == NULL)
     return line;
@@ -52,32 +64,31 @@ std::string StreamEditor::changePattern(std::string line, const char *s1,
   if (s1[0] == '\0' || s2[0] == '\0')
     return line;
 
-  std::size_t s1_len = tm_strlen(s1);
   std::size_t s2_len = tm_strlen(s2);
 
-  for (std::string::iterator it = line.begin(), end = line.end(); it != end;
-       ++it) {
-    char c = *it;
-    for (std::size_t i = 0; i < s1_len; i++) {
-      if (s1[i] == c)
-        counter++;
-    }
-  }
+  line.find(s1, index);
+
+  if (index == std::string::npos)
+    return line;
+
+  tm_replace(index, s2_len, (char *)s1, s2);
+  return s2;
 }
 
 void StreamEditor::getFileContent() {
   std::fstream ifs(StreamEditor::inputFile, std::ios::in);
-  std::fstream ofs(StreamEditor::outputFile, std::ios::out);
+  std::fstream ofs(strcat((char *)StreamEditor::inputFile, ".replace"),
+                   std::ios::in | std::ios::out | std::ios::app);
   std::string line = "";
 
-  if (!ifs.is_open() || !ofs.is_open()) {
+  if (!ifs.is_open()) {
     std::cerr << "Error: unable to open file." << '\n';
     return;
   }
 
   while (ifs.good() && std::getline(ifs, StreamEditor::lines)) {
     line = StreamEditor::lines;
-
+    StreamEditor::changePattern(line, StreamEditor::s1, StreamEditor::s2);
     ofs << StreamEditor::lines << '\n';
   }
 
